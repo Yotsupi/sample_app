53be2fd3d4faa84c782da2dea3124f9a405d8b11
EC2 Default User <ec2-user@ip-172-31-61-116.ec2.internal>
2021-08-06 13:38:35 +0000

    Create_imagepostng

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'c865180c8e153a138f0aeb665889c7193e9ed47ba87429d2da6dcf130b6f3f63935daabf085a3db30b8577b257793a463a89e510721eca4c13db706b44175c12'
